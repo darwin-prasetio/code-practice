34e7ad232d9cf029dcd3a8aa3252c5371dc337c0
darwin-prasetio <prasetiodarwin@gmail.com>
2020-06-16 23:55:50 +0700

    leetcode judge-square-sum

---
+++ judge-square-sum.java
@@ -0,0 +1,15 @@
+// problem is at https://leetcode.com/problems/sum-of-square-numbers/submissions/
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+      Set<Integer> set = new HashSet<>();
+      for (int a = 0; a <= Math.sqrt(c); ++a) {
+        int square = a * a;
+        set.add(square);
+        if (set.contains(c-square)) {
+          return true;
+        }
+      }
+      return false;
+    }
+}
