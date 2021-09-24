da250cfa77b22f78a65efae2855345e8c67c8c53
darwin-prasetio <prasetiodarwin@gmail.com>
2021-09-24 20:34:31 +0700

    Create SearchMatrix.java

---
+++ leetcode/SearchMatrix.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; ++i) {
+            if (target <= matrix[i][matrix[i].length - 1]) {
+                return Arrays.binarySearch(matrix[i], target) >= 0;
+            }
+        }
+        return false;
+    }
+}
